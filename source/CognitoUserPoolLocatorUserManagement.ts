0ad4ee8efcf2fc6ccd594d1ae3f441ff5e89b538
Andrew Regier <aregier@regiernet.com>
2018-11-27 12:25:27 -0600

    Fix issue #4 -  error information being swallowed on failed user lookup

--- source/CognitoUserPoolLocatorUserManagement.ts
+++ source/CognitoUserPoolLocatorUserManagement.ts
@@ -13,14 +13,18 @@ export class CognitoUserPoolLocatorUserManagement implements ICognitoUserPoolLoc
     public getPoolForUsername(userName: string): Promise<ICognitoUserPoolApiModel> {
         return new Promise(async function (resolve, reject) {
             const api = new UserManagementApi(process.env.USER_MANAGEMENT_URI, this.region, { type: 'None' });
-            const response: any = await api.getUserPoolByUserName(userName);
-            const result: ICognitoUserPoolApiModel = {
-                ClientId: response.data.cognito_app_client_id,
-                IdentityPoolId: response.data.cognito_identity_pool_id,
-                UserPoolId: response.data.cognito_user_pool_id
-            };
 
-            resolve(result);
+            try {
+                const response: any = await api.getUserPoolByUserName(userName);
+                const result: ICognitoUserPoolApiModel = {
+                    ClientId: response.data.cognito_app_client_id,
+                    IdentityPoolId: response.data.cognito_identity_pool_id,
+                    UserPoolId: response.data.cognito_user_pool_id
+                };
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
         }.bind(this));
     }
 }
