3474a90c7ee31170c1a52d09b27ddb4b87d9e8c8
Matt Cook <matt@mattcook.xyz>
2019-09-24 09:13:04 -0500

    Coverage

--- test/unit/UserManagementApi.spec.ts
+++ test/unit/UserManagementApi.spec.ts
@@ -10,7 +10,13 @@ describe('UserManagementApi', () => {
 
     let sandbox: sinon.SinonSandbox;
     let api: UserManagementApi;
-    let tenant_id: string;
+    const tenant_id: string = 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee';
+    const id: string = 'idididid';
+    const tenantName: string = 'blah';
+    const userName: string = 'testusername';
+    const password: string = 'testpassword';
+    const firstName: string = 'blah';
+    const lastName: string = 'blah';
     let client: {
         invokeApi: sinon.SinonStub
     };
@@ -18,7 +24,6 @@ describe('UserManagementApi', () => {
     beforeEach(() => {
         sandbox = sinon.createSandbox();
         client = { invokeApi: sinon.stub() };
-        tenant_id = 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee';
         api = new UserManagementApi('blah', 'blah', { type: 'None' } as ApiCredentials, client);
     });
 
@@ -35,6 +40,102 @@ describe('UserManagementApi', () => {
             expect(client.invokeApi.args[0][2]).to.deep.equal('POST');
             expect(client.invokeApi.args[0][3]).to.deep.equal({});
             expect(client.invokeApi.args[0][4]).to.deep.equal({ tenant_id });
+        });
+    });
+
+    describe('deleteUserPool', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.deleteUserPool(id);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/admin/userpools/' + id);
+            expect(client.invokeApi.args[0][2]).to.deep.equal('DELETE');
+            expect(client.invokeApi.args[0][3]).to.deep.equal({});
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
+        });
+    });
+
+    describe('getUserPools', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.getUserPools();
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/userpools');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('GET');
+            // Need to check that it has bearer token too
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
+        });
+    });
+
+    describe('createUser', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.createUser(tenant_id, userName, password, firstName, lastName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/admin/users');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('POST');
+            expect(client.invokeApi.args[0][3]).to.deep.equal({});
+            expect(client.invokeApi.args[0][4]).to.deep.equal({ tenant_id, userName, password, firstName, lastName });
+        });
+    });
+    
+    describe('registerTenant', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.registerTenant(tenantName, userName, firstName, lastName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/tenant/register');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('POST');
+            expect(client.invokeApi.args[0][3]).to.deep.equal({});
+            expect(client.invokeApi.args[0][4]).to.deep.equal({ tenantName, userName, firstName, lastName });
+        });
+    });
+
+    describe('deleteUser', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.deleteUser(userName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/admin/users/' + encodeURIComponent(userName));
+            expect(client.invokeApi.args[0][2]).to.deep.equal('DELETE');
+            expect(client.invokeApi.args[0][3]).to.deep.equal({});
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
+        });
+    });
+
+    describe('getPoolByUserName', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.getUserPoolByUserName(userName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/users/' + encodeURIComponent(userName) + '/pool');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('GET');
+            expect(client.invokeApi.args[0][3]).to.deep.equal({});
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
+        });
+    });
+
+    describe('getUserInfo', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.getUserInfo(userName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/users/' + encodeURIComponent(userName) + '/info');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('GET');
+            // Need to check that it has bearer token too
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
+        });
+    });
+
+    describe('getUsers', () => {
+        it('Should invoke api with correct args', async () => {
+            await api.getUsers(firstName);
+            expect(client.invokeApi.calledOnce).to.be.true;
+            expect(client.invokeApi.args[0][0]).to.deep.equal({});
+            expect(client.invokeApi.args[0][1]).to.deep.equal('/users');
+            expect(client.invokeApi.args[0][2]).to.deep.equal('GET');
+            // Need to check that it has bearer token too
+            expect(client.invokeApi.args[0][4]).to.deep.equal({});
         });
     });
 });
